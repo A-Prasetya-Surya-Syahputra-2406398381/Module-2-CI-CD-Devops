599d0bbc1d675b8c385811b7f3fea84b82096371
prasetyasurya-ui <prasetya.surya@ui.ac.id>
2026-02-13 12:59:36 +0700

    feat: add implicit wait

--- src/test/java/id/ac/ui/cs/advprog/eshop/functional/CreateProductFunctionalTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/functional/CreateProductFunctionalTest.java
@@ -53,11 +53,13 @@ public class CreateProductFunctionalTest {
 
     @Test
     void testCreateProductIsSuccessful(ChromeDriver driver) {
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
 
         driver.get(baseUrl + "/product/create");
 
-        WebElement nameInput = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("nameInput")));
+        WebElement nameInput = wait.until(ExpectedConditions.elementToBeClickable(By.id("nameInput")));
         nameInput.sendKeys("Sampo Cap Bambang");
 
         driver.findElement(By.id("quantityInput")).sendKeys("100");
@@ -65,7 +67,8 @@ public class CreateProductFunctionalTest {
 
         wait.until(ExpectedConditions.urlContains("/product/list"));
 
-        WebElement tableCell = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//td[contains(text(), 'Sampo Cap Bambang')]")));
+        WebElement tableCell = wait.until(ExpectedConditions.presenceOfElementLocated(
+                By.xpath("//td[text()='Sampo Cap Bambang']")));
 
         assertTrue(tableCell.isDisplayed());
     }
